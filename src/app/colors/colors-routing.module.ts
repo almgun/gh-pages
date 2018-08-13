97dc5241a91a133df9f0495824f66d6ab958bcdf
Gunnar Almas <gunnar.almaas@brunvoll.no>
2018-08-13 20:28:54 +0200

    prepare for child routes

--- src/app/colors/colors-routing.module.ts
+++ src/app/colors/colors-routing.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import  {RedComponent}  from './red/red.component';
-import  {BlueComponent}  from './blue/blue.component';
-import  {GreenComponent}  from './green/green.component';
-import  {ColorListComponent}  from './color-list/color-list.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
+import { GreenComponent } from './green/green.component';
+import { ColorListComponent } from './color-list/color-list.component';
 import { ColorDetailComponent } from './color-detail/color-detail.component';
 
 const routes: Routes = [
 
-  { path: 'color-list', component: ColorListComponent },
-  { path: 'color-detail/:id', component: ColorDetailComponent },
-  { path: 'red', component: RedComponent },
-  { path: 'blue', component: BlueComponent },
-  { path: 'green', component: GreenComponent }
+  {
+    path: 'color-list',
+    component: ColorListComponent
+  },
+  {
+    path: 'color-detail/:id',
+    component: ColorDetailComponent
+  }
+
 
 ];
 
 @NgModule({
-  exports: [ RouterModule ],
-  imports: [ RouterModule.forChild(routes) ],
+  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
   declarations: []
 })
 export class ColorsRoutingModule { }
